8c84113df3b4bd70cc598f3e8a5c41d8121bb9db
Vasily Borovkov <redison00x@mail.ru>
2024-03-01 18:56:38 +0300

    contains

--- src/test/java/Sravni.java
+++ src/test/java/Sravni.java
@@ -8,7 +8,11 @@ public class Sravni {
     public void avtorizatsiya() {
         open("https://www.sravni.ru/");
         $(new ByTextCaseInsensitive("Войти")).click();
-        $x("//a[contains(@href, '/signin/email')]").click();
+        switchTo().window(1);
+        $x("//a[contains(@href, \"signin/email\")]").click();
+        $x("//input[@name='email']").setValue("redison00x@mail.ru");
+        $x("//input[@name='password']").setValue("bestPROGRAMMIST228");
+        $x("//span[@class=\"_hp2bt3\"]").click();
 
     }
 }
