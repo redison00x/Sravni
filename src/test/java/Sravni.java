d851295ed7a24f69c370537b0e96f4e5a1a0f172
Vasily Borovkov <redison00x@mail.ru>
2024-03-30 16:24:58 +0300

    v7

--- src/test/java/Sravni.java
+++ src/test/java/Sravni.java
@@ -1,8 +1,11 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.selector.ByTextCaseInsensitive;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Configuration.timeout;
 import static com.codeborne.selenide.Selenide.*;
 import static org.testng.Assert.assertEquals;
 
@@ -21,10 +24,26 @@ public class Sravni {
         $x("//div[@class=\"page_wrapper__Yk7Um\"]").shouldBe(Condition.visible);
     }
     @Test
-    public void osagoSber() {
-        timeout = 15000;
-        open("https://osago.sberbank.ru/");
-        sleep(1500);
-        assertEquals (title(), "Оформить ОСАГО от Сбербанка", "Не тудой приперся");
+    public void osago() {
+        Configuration.pageLoadTimeout = 3000;
+        open("https://www.rgs.ru/auto/osago");
+        assertEquals (title(), "ОСАГО онлайн: калькулятор еОСАГО", "Не тудой приперся");
+        Selenide.Wait().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='action-item btn--basic']"))).click();
+
+        //switchTo().frame($x("//*[@id='RESOLUTE_INSURANCE']"));
+
+        //$x("//div[contains(text(),'Легковые ТС')]").click();
+        //$x("//div[text()='Легковые ТС (B)']").click();
+
+
+        //$x("//input[@aria-invalid='true']").setValue("89,00");
+
+
+        //$(new ByTextCaseInsensitive("л.с.")).click();
+        //$x("//div[text()='л.с.']").click();
+
+
+
+
     }
 }
